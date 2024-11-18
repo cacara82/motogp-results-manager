cd7f2b1358b66177ca292d2abf0d5acad66d79cd
cacara82 <cacara890@gmail.com>
2024-11-18 10:54:36 +0100

    functionality of fetchData and modified readme

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 ## MotoGP Results Manager
-A MotoGP stats-driven data manager, managed by APIs
+A MotoGP stats-driven data manager, managed by CSV dataset files from Kaggle.
 Made by Carlos Carvajal (@cacara82)
+
+## Usage
+1.- You will need poetry to run this project: poetry install
+2.- Update the imports: poetry update
+3.- You can now run "app/main.py" to start
+
+## Important
+Do NOT rename the dataset files, as the project uses their default names to load
+them into memory. If you do, when you fetch new data you will have file duplicates with
+different names.
+
--- app/utils.py
+++ app/utils.py
@@ -4,21 +4,21 @@ import os
 
 def fetchData():
     """
-    Fetches the dataset from Kaggle and moves it to the 'motogp_library' folder
-    within the project directory.
+    This function fetches the most recent data for the dataset from Kaggle and 
+    moves it to the 'motogp_library' folder within the project directory.
     """
-    # Obtener la ruta del proyecto
-    project_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    # We get the root of the project dir
+    project_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 
-    # Ruta destino en el proyecto
+    # We get the destination path in the project dir and create it if it doesn't exist
     dest_path = os.path.join(project_dir, "motogp_library")
     os.makedirs(dest_path, exist_ok=True)
 
-    # Descargar el dataset
-    dataset_path = kh.dataset_download("alrizacelk/moto-gp-world-championship19492022")
+    # We download the last version of the dataset from Kaggle
+    dataset_path = kh.dataset_download("alrizacelk/moto-gp-world-championship19492022", force_download=True)
     print(f"Dataset downloaded to: {dataset_path}")
 
-    # Mover todos los archivos descargados al directorio del proyecto
+    # We move all the files downloaded in their path to the path in the project
     for file_name in os.listdir(dataset_path):
         full_file_path = os.path.join(dataset_path, file_name)
         shutil.move(full_file_path, dest_path)
